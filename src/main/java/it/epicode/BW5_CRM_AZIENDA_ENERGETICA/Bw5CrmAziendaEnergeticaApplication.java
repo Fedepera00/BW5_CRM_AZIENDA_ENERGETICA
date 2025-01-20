5d5c80caf2bc07ef1fac6e857e96256665fc5387
Federico Peralta <federico.peralta91@gmail.com>
2025-01-20 11:30:40 +0100

    first commit

---
+++ src/main/java/it/epicode/BW5_CRM_AZIENDA_ENERGETICA/Bw5CrmAziendaEnergeticaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.BW5_CRM_AZIENDA_ENERGETICA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw5CrmAziendaEnergeticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw5CrmAziendaEnergeticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/BW5_CRM_AZIENDA_ENERGETICA/Bw5CrmAziendaEnergeticaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.BW5_CRM_AZIENDA_ENERGETICA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5CrmAziendaEnergeticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
