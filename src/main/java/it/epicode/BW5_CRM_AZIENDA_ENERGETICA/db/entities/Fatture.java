5fe755783f721d20d1dc58c71379c4b02f7644a2
Federico Peralta <federico.peralta91@gmail.com>
2025-01-20 11:50:59 +0100

    update

---
+++ src/main/java/it/epicode/BW5_CRM_AZIENDA_ENERGETICA/db/entities/Fatture.java
@@ -0,0 +1,14 @@
+package it.epicode.BW5_CRM_AZIENDA_ENERGETICA.db.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Fatture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+
+}
